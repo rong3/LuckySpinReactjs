87ecc62200996b7f557778d85e39ab27c89b4bcc
trieu.tran <trieu.tran@alpaca.vn>
2022-04-01 10:40:36 +0700

    sync

--- pages/404.js
+++ pages/404.js
@@ -13,7 +13,7 @@ export default function NotFoundException() {
                     <div className="col-12 d-flex align-items-center justify-content-center">
                         <div className="col-md-4 col-10 p-0">
                             <div className="card-header bg-transparent border-0">
-                                <h3 className="text-uppercase text-center">The page you were looking for doesn't exist</h3>
+                                <h3 className="text-uppercase text-center">The page you were looking for doesnt exist</h3>
                             </div>
                         </div>
                     </div>
