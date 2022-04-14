82aac70d3eb10014c054724510b78a326b944a4d
trieu.tran <trieu.tran@alpaca.vn>
2022-04-14 11:18:54 +0700

    sync

--- shared/packages/control/grid/datagrid.js
+++ shared/packages/control/grid/datagrid.js
@@ -4,6 +4,16 @@ import { colors, TextField, withStyles } from "@material-ui/core";
 import { Height } from '@material-ui/icons';
 const styles = () => ({
     root: {
+        dataGrid: {
+            background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
+            borderRadius: 3,
+            border: 0,
+            color: "white",
+            height: 48,
+            padding: "0 30px",
+            boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)",
+            width: "640px"
+        }
         // '&.MuiDataGrid-root .MuiDataGrid-columnHeader:focus, &.MuiDataGrid-root .MuiDataGrid-cell:focus': {
         //     outline: 'none !important',
         // },
@@ -29,31 +39,36 @@ const styles = () => ({
     }
 });
 function DataGridControl({ rows, columns, pageSize, ...rest }) {
-    const [pageSizeProps, setPageSizeProps] = React.useState(pageSize ?? 5);
+    const [pageSizeProps, setPageSizeProps] = React.useState(pageSize ?? 20);
+    const classes = styles();
     // https://mui.com/api/data-grid/data-grid/
     // filter custom
     // https://mui.com/components/data-grid/filtering/
     return (
-        <DataGrid
-            rows={rows}
-            autoHeight
-            columns={columns}
-            rowHeight={45}
-            headerHeight={45}
-            pageSize={pageSizeProps}
-            rowsPerPageOptions={[5, 10, 20, 50, 100]}
-            onPageSizeChange={(newPageSize) => setPageSizeProps(newPageSize)}
-            localeText={{
-                toolbarDensity: 'Size',
-                toolbarDensityLabel: 'Size',
-                toolbarDensityCompact: 'Small',
-                toolbarDensityStandard: 'Medium',
-                toolbarDensityComfortable: 'Large',
-            }}
-            // filterMode="server"
-            // onFilterModelChange={handleFilterModelChange}
-            {...rest}
-        />
+        <div style={{ width: "100%" }}>
+            <DataGrid
+                rows={rows}
+                autoHeight
+                columns={columns}
+                rowHeight={45}
+                disableExtendRowFullWidth={false}
+                scrollbarSize={50}
+                headerHeight={45}
+                pageSize={pageSizeProps}
+                rowsPerPageOptions={[5, 10, 20, 50, 100]}
+                onPageSizeChange={(newPageSize) => setPageSizeProps(newPageSize)}
+                localeText={{
+                    toolbarDensity: 'Size',
+                    toolbarDensityLabel: 'Size',
+                    toolbarDensityCompact: 'Small',
+                    toolbarDensityStandard: 'Medium',
+                    toolbarDensityComfortable: 'Large',
+                }}
+                // filterMode="server"
+                // onFilterModelChange={handleFilterModelChange}
+                {...rest}
+            />
+        </div>
     );
 }
 export default withStyles(styles)(DataGridControl);
