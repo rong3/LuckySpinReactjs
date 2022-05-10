b334ad508c9b84c4cabf6dc881eb3c338050989c
trieu.tran <trieu.tran@alpaca.vn>
2022-05-10 21:40:40 +0700

    sync

--- component/embedded/lucky-spin/luckySpin.js
+++ component/embedded/lucky-spin/luckySpin.js
@@ -484,11 +484,17 @@ const LuckySpinComponent = (props) => {
                     <>
                         <header>
                             <nav class="navbar d-flex align-items-center justify-content-between">
-                                <div class="navbar-brand"> <a href=""> <img src="/asset/images/luckyspin/theme/HDBank_OPT2/background/logo2.png" alt="" /></a></div>
+                                <div class="navbar-brand"> <a> <img src="/asset/images/luckyspin/theme/HDBank_OPT2/background/logo2.png" alt="" /></a></div>
                                 <div class="navbar-right">
-                                    <div class="dropdown"><a class="navbar-avatar" href="#">
+                                    <div class="dropdown"><a class="navbar-avatar dropdown-toggle" href="#">
                                         <img class="avatar" src="/asset/images/luckyspin/theme/HDbank/background/avarta.png" alt="" />
                                         <p style={{ color: "#000" }}>&nbsp; Xin chào, {renderName()}</p></a>
+                                        <ul class="dropdown-menu">
+                                            <li onClick={() => {
+                                                CookieHelper.removeCookie("access_token");
+                                                window.location.reload();
+                                            }}><a>Đăng xuất</a></li>
+                                        </ul>
                                     </div>
                                 </div>
                             </nav>
